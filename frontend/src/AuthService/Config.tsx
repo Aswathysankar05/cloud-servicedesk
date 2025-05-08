49add652c20478f414861029930d8283d756f4c3
Aswathysankar05 <aswathy.s@cloudplusinfotech.com>
2025-05-08 12:27:55 +0530

    Update Config.tsx

--- frontend/src/AuthService/Config.tsx
+++ frontend/src/AuthService/Config.tsx
@@ -1,2 +1,3 @@
 //export const BASE_URL = 'https://servicedesk-dev.cloudplusinfotech.com/api';
+//export const BASE_URL = 'http://servicedesk.cloudplusinfotech.com:8080/api'; 
 export const BASE_URL = 'http://servicedesk.cloudplusinfotech.com:8080/api'; 
